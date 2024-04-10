e6faf8e07b789f350d5aa5efeabd5195d6907d54
Rahaman <rahaman@sidpik.com>
2024-04-10 22:35:52 +0530

    yml updated successfully

--- src/main/java/sportify/backend/api/service/scoreCard/IplScoreCardApiService.java
+++ src/main/java/sportify/backend/api/service/scoreCard/IplScoreCardApiService.java
@@ -5,7 +5,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import sportify.backend.api.dto.scoreCard.IplScoreCardApiDto;
 
-@Service
 public interface IplScoreCardApiService {
   IplScoreCardApiDto createEntity(String dateTime)throws Exception;
   IplScoreCardApiDto getEntityById(String id) throws Exception;
